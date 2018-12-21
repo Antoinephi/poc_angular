9d665ed4b7c35e1d8a2fc9594c10eb326d670a89
Antoine Philippe <antoine.philippe62@gmail.com>
2018-12-21 14:04:00 +0100

    poc: first test-implem of OSM

---
+++ src/app/stations-map/stations-map.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+declare var ol: any;
+
+@Component({
+  selector: 'app-stations-map',
+  templateUrl: './stations-map.component.html',
+  styleUrls: ['./stations-map.component.scss']
+})
+export class StationsMapComponent implements OnInit {
+  latitude: number = 50.6336555;
+  longitude: number = 3.0587139;
+  map: any;
+
+  constructor() { }
+  
+  ngOnInit() {
+    this.map = new ol.Map({
+      target: 'map',
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
+        })
+      ],
+      view: new ol.View({
+        center: ol.proj.fromLonLat([this.longitude, this.latitude]),
+        zoom: 18
+      })
+    });
+  }
+
+}
