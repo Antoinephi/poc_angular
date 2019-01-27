478912d20c18a5555f8f2cd36fee2a85140308a3
Antoine Philippe <antoine.philippe@zenika.com>
2019-01-28 00:31:01 +0100

    fix(Layout): updated style layout for main components, header & footer. Viewport still needs a fix when using chrome mobile and toggling full screen (scroll)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Host, HostListener } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
@@ -13,6 +13,18 @@ export class AppComponent {
     .pipe(
       map(result => result.matches)
     );
+  public dynamicHeight = window.innerHeight;
 
-  constructor(private breakpointObserver: BreakpointObserver) { }
+  constructor(private breakpointObserver: BreakpointObserver) {
+  }
+
+  // @HostListener('window:resize', ['$event'])
+  // private calcViewport(event) {
+  //   // this.dynamicHeight = window.outerHeight;
+
+  //   const vh = window.innerHeight * 0.01;
+  //   document.documentElement.style.setProperty('--vh', `${vh}px`);
+  //   console.log('resizing', vh);
+
+  // }
 }
